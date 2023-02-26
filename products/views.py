28a4810b56b759ed8a67de46e6b94ba1e802205c
Noor ul ain Ibrahim <noorulainibrahim75@gmail.com>
2023-02-26 15:19:34 +0400

    viewsets are tested

--- products/views.py
+++ products/views.py
@@ -3,26 +3,48 @@ from .models import AllProducts
 from .serializers import AllProductSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.exceptions import APIException
 
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-        products = AllProducts.objects.all()
-        ser = AllProductSerializer(products, many=True)
-        return Response(ser.data)
+        try:
+            products = AllProducts.objects.all()
+            ser = AllProductSerializer(products, many=True)
+            return Response(ser.data)
+        except:
+            return Response({'message': 'something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def create(self, request):
-        product_data = AllProductSerializer(data=request.data)
-        product_data.is_valid(raise_exception=True)
-        product_data.save()
-        return Response(product_data.data, status=status.HTTP_201_CREATED)
+        try:
+            product_data = AllProductSerializer(data=request.data)
+            product_data.is_valid(raise_exception=True)
+            product_data.save()
+            return Response(product_data.data, status=status.HTTP_201_CREATED)
+        except:
+            return Response({'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None):
-        pass
+        try:
+            product = AllProducts.objects.get(pk=pk)
+            ser = AllProductSerializer(product)
+            return Response(ser.data)
+        except:
+            return Response({'message': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk=None):
-        pass
+        try:
+            product = AllProducts.objects.get(id=pk)
+            product_data = AllProductSerializer(product, data=request.data, partial=True)
+            product_data.is_valid(raise_exception=True)
+            product_data.save()
+            return Response(product_data.data, status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response({'message': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        pass
+        try:
+            product = AllProducts.objects.get(id=pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({'message': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
